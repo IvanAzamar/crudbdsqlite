88a998d5bc0bda07e69f89e96d114ff0fa318ed6
IvanAzamar <81318326+IvanAzamar@users.noreply.github.com>
2024-05-30 19:10:40 -0600

    Update ConexionSQLiteHelper.java
    oye ivan modifique tu codigo

--- app/src/main/java/com/example/crudbdsqlite/ConexionSQLiteHelper.java
+++ app/src/main/java/com/example/crudbdsqlite/ConexionSQLiteHelper.java
@@ -23,6 +23,6 @@ public class ConexionSQLiteHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS usuario");
         onCreate(db);
-
+//modifique
     }
 }
